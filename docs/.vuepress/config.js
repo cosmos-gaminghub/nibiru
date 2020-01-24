680cd8fbca1d1ac5e520cfa92cfc82a77efeeed8
EG-easy <1018eguchi@gmail.com>
2020-01-24 23:16:48 +0900

    [add] vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    title: "Nibiru",
+    description: "Documentation for the Cosmos Gaming Hub",
+    head: [],
+    markdown: {
+        lineNumbers: true,
+    },
+    plugins: [],
+    themeConfig: {
+        repo: "cosmos-gaminghub/nibiru",
+        editLinks: true,
+        docsDir: "docs",
+        docsBranch: "master",
+        editLinkText: 'Edit this page on Github',
+        lastUpdated: true,
+        logo: "/assets/logo.png",
+        nav: [
+            {text: "Website", link: "https://cosmosgaminghub.com", target: "_blank"},
+        ],
+        sidebarDepth: 2,
+        sidebar: [
+            {
+                title: "Testnets",
+                collapsable: false,
+                children: [
+                    ["testnets/overview", "Overview"]
+                ]
+            }
+        ],
+    }
+};
