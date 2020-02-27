8c97c17439d722cf766a750bd71f047fbd131521
EG-easy <1018eguchi@gmail.com>
2020-02-27 16:10:57 +0900

    [add]cloudfront command

--- docs/deploy-s3.sh
+++ docs/deploy-s3.sh
@@ -2,3 +2,4 @@
 
 aws s3 rm s3://$DOCS_BUCKET/ --recursive
 aws s3 cp ./.vuepress/dist s3://$DOCS_BUCKET/ --recursive
+aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths '/*'
