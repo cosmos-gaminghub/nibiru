614773bf111e8c6880efc15ad8f3e5a38e95f08d
PhamAnhHoang <hoangpa@paditech.com>
2021-05-27 14:03:53 +0700

    Update

---
+++ cmd/nbrd/cmd/serve.go
@@ -0,0 +1,68 @@
+package cmd
+
+import (
+	"github.com/cosmos-gaminghub/nibiru/pkg/chaincmd"
+	"github.com/cosmos-gaminghub/nibiru/services/chain"
+	"github.com/spf13/cobra"
+)
+
+const flagForceReset = "force-reset"
+const flagResetOnce = "reset-once"
+const flagConfig = "config"
+
+var appPath string
+
+type KeyringBackend string
+
+const (
+	KeyringBackendTest KeyringBackend = "test"
+)
+
+// NewServe creates a new serve command to serve a blockchain.
+func NewServe() *cobra.Command {
+	c := &cobra.Command{
+		Use:   "serve",
+		Short: "Start a blockchain node in development",
+		Long:  "Start a blockchain node with automatic reloading",
+		Args:  cobra.ExactArgs(0),
+		RunE:  serveHandler,
+	}
+	//c.Flags().AddFlagSet(flagSetHome())
+	// c.Flags().StringVarP(&appPath, "path", "p", "", "Path of the app")
+	// c.Flags().BoolP("verbose", "v", false, "Verbose output")
+	// c.Flags().BoolP(flagForceReset, "f", false, "Force reset of the app state on start and every source change")
+	// c.Flags().BoolP(flagResetOnce, "r", false, "Reset of the app state on first start")
+	// c.Flags().Bool(flagRebuildProtoOnce, false, "Enables proto code generation for 3rd party modules")
+	c.Flags().StringP(flagConfig, "c", "", "Starport config file (default: ./config.yml)")
+
+	return c
+}
+
+func serveHandler(cmd *cobra.Command, args []string) error {
+	chainOption := []chain.Option{
+		chain.LogLevel(logLevel(cmd)),
+		chain.KeyringBackend(chaincmd.KeyringBackendTest),
+	}
+
+	// if isRebuildProtoOnce {
+	// 	chainOption = append(chainOption, chain.EnableThirdPartyModuleCodegen())
+	// }
+
+	// check if custom config is defined
+	config, err := cmd.Flags().GetString(flagConfig)
+	if err != nil {
+		return err
+	}
+	if config != "" {
+		chainOption = append(chainOption, chain.ConfigFile(config))
+	}
+
+	// create the chain
+	c, err := newChainWithHomeFlags(cmd, appPath, chainOption...)
+	if err != nil {
+		return err
+	}
+
+	return c.Serve(cmd.Context())
+	// return nil
+}
--- go.mod
+++ go.mod
@@ -1,14 +1,22 @@
 module github.com/cosmos-gaminghub/nibiru
 
-go 1.15
+go 1.16
 
 require (
 	github.com/btcsuite/btcd v0.21.0-beta
 	github.com/btcsuite/btcutil v1.0.2
 	github.com/cosmos/cosmos-sdk v0.42.4
 	github.com/ethereum/go-ethereum v1.10.3
+	github.com/go-git/go-git/v5 v5.1.0
+	github.com/goccy/go-yaml v1.8.9
+	github.com/gookit/color v1.4.2
 	github.com/gorilla/mux v1.8.0
+	github.com/imdario/mergo v0.3.12
+	github.com/mattn/go-zglob v0.0.3
+	github.com/otiai10/copy v1.4.2
+	github.com/pelletier/go-toml v1.8.1
 	github.com/pkg/errors v0.9.1
+	github.com/radovskyb/watcher v1.0.7
 	github.com/rakyll/statik v0.1.7
 	github.com/spf13/cast v1.3.1
 	github.com/spf13/cobra v1.1.3
@@ -18,6 +26,8 @@ require (
 	github.com/tendermint/go-amino v0.16.0
 	github.com/tendermint/tendermint v0.34.10
 	github.com/tendermint/tm-db v0.6.4
+	golang.org/x/mod v0.4.2
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
 )
 
 replace (
