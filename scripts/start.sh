93b497350242e886236a40a078424f2674affad9
EG-easy <1018eguchi@gmail.com>
2021-09-20 22:08:13 +0900

    fix genesis test workflow

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
+
 NETWORK=nibiru-2000
 DAEMON=nibirud
 HOME_DIR=~/.nibiru
 CONFIG=~/.nibiru/config
 TOKEN_DENOM=game
 
-rm $CONFIG/genesis.json
-rm -rf $CONFIG/gentx && mkdir $CONFIG/gentx
+rm -rf $HOME_DIR
 
-# Initialize configuration files and genesis file
 $DAEMON init $NETWORK --chain-id $NETWORK
+$DAEMON keys add eg
+$DAEMON add-genesis-account $($DAEMON keys show eg -a) 100000000000000$TOKEN_DENOM
 
-# fix denom in genesis.json
 sed -i "s/\"stake\"/\"$TOKEN_DENOM\"/g" $HOME_DIR/config/genesis.json
 
-# Copy the `Address` output here and save it for later use
-$DAEMON keys add jack
+$DAEMON gentx eg 50000000000000$TOKEN_DENOM --chain-id=$NETWORK --commission-max-change-rate=0.1 --commission-max-rate=1 --commission-rate=0.1 --moniker=eg-validator
 
-# Add both accounts, with coins to the genesis file
-$DAEMON add-genesis-account $($DAEMON keys show jack -a) 100000000000000$TOKEN_DENOM
-
-$DAEMON gentx jack 10000000000000$TOKEN_DENOM --commission-rate=0.1 --commission-max-rate=1 --commission-max-change-rate=0.1 --pubkey $($DAEMON tendermint show-validator) --chain-id=$NETWORK
 
 $DAEMON collect-gentxs
 
