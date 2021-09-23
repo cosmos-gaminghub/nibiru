ddff297dcbe5730b96bfc13656f5fc555e3e8f86
EG-easy <1018eguchi@gmail.com>
2021-09-24 00:09:01 +0900

    fix script for test

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,12 +9,12 @@ TOKEN_DENOM=game
 rm -rf $HOME_DIR
 
 $DAEMON init $NETWORK --chain-id $NETWORK
-$DAEMON keys add eg
-$DAEMON add-genesis-account $($DAEMON keys show eg -a) 100000000000000$TOKEN_DENOM
+$DAEMON keys add eg --keyring-backend=test
+$DAEMON add-genesis-account $($DAEMON keys show eg -a --keyring-backend=test) 100000000000000$TOKEN_DENOM
 
 sed -i "s/\"stake\"/\"$TOKEN_DENOM\"/g" $HOME_DIR/config/genesis.json
 
-$DAEMON gentx eg 50000000000000$TOKEN_DENOM --chain-id=$NETWORK --commission-max-change-rate=0.1 --commission-max-rate=1 --commission-rate=0.1 --moniker=eg-validator
+$DAEMON gentx eg 50000000000000$TOKEN_DENOM --chain-id=$NETWORK --commission-max-change-rate=0.1 --commission-max-rate=1 --commission-rate=0.1 --moniker=eg-validator --keyring-backend=test
 
 
 $DAEMON collect-gentxs
