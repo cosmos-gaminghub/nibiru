0d930eaf8285a29998b652f345cd07795e3c98a8
padi-dev-hoangpa <50005242+padi-dev-hoangpa@users.noreply.github.com>
2021-10-14 16:16:46 +0700

    Fix error unable to resolve type URL

--- x/signal/types/codec.go
+++ x/signal/types/codec.go
@@ -3,18 +3,20 @@ package types
 import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	// this line is used by starport scaffolding # 1
-	"github.com/cosmos/cosmos-sdk/types/msgservice"
 )
 
 func RegisterCodec(cdc *codec.LegacyAmino) {
 	// this line is used by starport scaffolding # 2
+	cdc.RegisterConcrete(&MsgCreateSignal{}, "signal/CreateSignal", nil)
 }
 
 func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
 	// this line is used by starport scaffolding # 3
-
-	msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)
+	registry.RegisterImplementations((*sdk.Msg)(nil),
+		&MsgCreateSignal{},
+	)
 }
 
 var (
