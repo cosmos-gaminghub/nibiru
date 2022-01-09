cffe02ca09f8ec141c7ab4bf4d54f43c7e08c78b
EG-easy <1018eguchi@gmail.com>
2022-01-09 22:46:42 +0900

    add app SetModuleVersionMap

--- app/app.go
+++ app/app.go
@@ -610,6 +610,7 @@ func (app *NibiruApp) InitChainer(ctx sdk.Context, req abci.RequestInitChain) ab
 	if err := tmjson.Unmarshal(req.AppStateBytes, &genesisState); err != nil {
 		panic(err)
 	}
+	app.UpgradeKeeper.SetModuleVersionMap(ctx, app.mm.GetVersionMap())
 	return app.mm.InitGenesis(ctx, app.appCodec, genesisState)
 }
 
